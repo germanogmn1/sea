b3c90d6ef0ca05ed4dcadeef4806564e4937e024
Germano Nicolini <germanogmn1@gmail.com>
2015-03-05 17:50:34 -0300

    Fix part of concurrency problems.

--- sea.go
+++ sea.go
@@ -1,89 +1,106 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "log"
-    "net/http"
-    "os/exec"
-    "io"
-    // "os"
-    // "bytes"
-    // "bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+	"sync"
+	// "os"
+	// "bytes"
+	// "bufio"
 )
 
 type OutputBuffer struct {
-    Content []byte
-    Stream chan []byte
-    Done bool
+	Content []byte
+	Stream  chan []byte
+	Done    bool
+	Mutex   sync.RWMutex
 }
 
 var cmdOut OutputBuffer
+var executed bool
 
+// io.Writer implementation
 func (o *OutputBuffer) Write(p []byte) (int, error) {
-    o.Stream <- p
-    o.Content = append(o.Content, p...)
-    return len(p), nil
+	select {
+	case o.Stream <- p:
+	default:
+	}
+	o.Mutex.Lock()
+	o.Content = append(o.Content, p...)
+	o.Mutex.Unlock()
+	return len(p), nil
 }
 
 func (o *OutputBuffer) ReadChunks() chan []byte {
-    response := make(chan []byte)
-    go func() {
-        if len(o.Content) > 0 {
-            response <- o.Content
-        }
-        if o.Done {
-            close(response)
-        } else {
-            for chunk := range o.Stream {
-                response <- chunk
-            }
-            close(response)
-        }
-    }()
-    return response
+	response := make(chan []byte)
+	go func() {
+		o.Mutex.RLock()
+		if len(o.Content) > 0 {
+			response <- o.Content
+		}
+		o.Mutex.RUnlock()
+		if o.Done {
+			close(response)
+		} else {
+			for chunk := range o.Stream {
+				response <- chunk
+			}
+			close(response)
+		}
+	}()
+	return response
 }
 
 func execFile(path string, output *OutputBuffer) {
-    cmd := exec.Command(path)
-    cmdout, err := cmd.StdoutPipe()
-    if err != nil {
-        log.Fatal(err)
-    }
-    err = cmd.Start()
-    if err != nil {
-        log.Fatal(err)
-    }
-    io.Copy(output, cmdout)
-    output.Done = true
-    cmd.Wait()
+	cmd := exec.Command(path)
+	cmdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(output, cmdout)
+	output.Done = true
+	cmd.Wait()
 }
 
 func stream(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("X-Content-Type-Options", "nosniff")
-    for chunk := range cmdOut.ReadChunks() {
-        w.Write(chunk)
-        w.(http.Flusher).Flush()
-    }
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	for chunk := range cmdOut.ReadChunks() {
+		w.Write(chunk)
+		w.(http.Flusher).Flush()
+	}
 }
 
 func main() {
-    cmdOut = OutputBuffer {
-        Content: make([]byte, 0),
-        Stream: make(chan []byte),
-        Done: false,
-    }
+	executed = false
+	cmdOut = OutputBuffer{
+		Content: make([]byte, 0),
+		Stream:  make(chan []byte),
+		Done:    false,
+	}
 
-    var port int
-    flag.IntVar(&port, "port", 8080, "HTTP port to listen")
-    flag.Parse()
+	var port int
+	flag.IntVar(&port, "port", 8080, "HTTP port to listen")
+	flag.Parse()
 
-    http.HandleFunc("/exec", func(w http.ResponseWriter, r *http.Request) {
-        go execFile("./Seafile", &cmdOut)
-        fmt.Fprintf(w, "running")
-    })
-    http.HandleFunc("/stream", stream)
-    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	http.HandleFunc("/exec", func(w http.ResponseWriter, r *http.Request) {
+		if executed {
+			fmt.Fprintf(w, "already running")
+		} else {
+			go execFile("./Seafile", &cmdOut)
+			fmt.Fprintf(w, "started")
+			executed = true
+		}
+	})
+	http.HandleFunc("/stream", stream)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
 /*
