37dab3b131232a17b22c3218221c8cd949708e34
Germano Nicolini <germanogmn1@gmail.com>
2015-03-12 18:02:05 -0300

    Messing with logging and panic handling.

--- http_wrapper.go
+++ http_wrapper.go
@@ -1,4 +1,4 @@
-package main
+package foo
 
 import (
 	"fmt"
@@ -12,19 +12,9 @@ type HTTPWrapper struct {
 }
 
 type writerWrapper struct {
-	w      http.ResponseWriter
-	status int
-}
-
-func (h *HTTPWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ww := writerWrapper{w, http.StatusOK}
-	start := time.Now()
-	log.Printf("Started %s %q", r.Method, r.RequestURI)
-	h.Handler.ServeHTTP(&ww, r)
-	duration := time.Since(start)
-	var milliseconds float64 = float64(duration.Nanoseconds()) / 10e6
-	log.Printf("Completed %d %s in %.2fms", ww.status,
-		http.StatusText(ww.status), milliseconds)
+	w       http.ResponseWriter
+	status  int
+	flushed bool
 }
 
 func (l *writerWrapper) Header() http.Header { fmt.Println("Header"); return l.w.Header() }
@@ -36,4 +26,33 @@ func (l *writerWrapper) WriteHeader(status int) {
 	l.w.WriteHeader(status)
 	l.status = status
 }
-func (l *writerWrapper) Flush() { l.w.(http.Flusher).Flush() }
+func (l *writerWrapper) Flush() {
+	l.w.(http.Flusher).Flush()
+	l.flushed = true
+}
+
+func (h *HTTPWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ww := writerWrapper{w, http.StatusOK}
+	start := time.Now()
+	log.Printf("Started %s %q", r.Method, r.RequestURI)
+
+	h.handlePanic(&ww, r)
+
+	duration := time.Since(start)
+	var milliseconds float64 = float64(duration.Nanoseconds()) / 10e6
+	log.Printf("Completed %d %s in %.2fms", ww.status,
+		http.StatusText(ww.status), milliseconds)
+}
+
+func (h *HTTPWrapper) handlePanic(ww *writerWrapper, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			if ww.flushed {
+				// TODO: response already sent to client, now what?
+			} else {
+				// TODO: get stack trace, render error page
+			}
+		}
+	}()
+	h.Handler(ww, r)
+}
